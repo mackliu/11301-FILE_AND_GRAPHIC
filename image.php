f0b73208051467340c61310282de8c424741f511
kai <cgk0910@gmail.com>
2024-08-16 11:17:40 +0800

    繪製邊框

--- image.php
+++ image.php
@@ -69,9 +69,12 @@
             // 獲取原始圖片大小
             list($width, $height) = getimagesize($target_file);
 
-            // 計算新尺寸
-            $new_width = $width * 0.5;
-            $new_height = $height * 0.5;
+            // 計算邊框的寬度，預設邊框寬度佔總寬度的10%
+            $border_size = round($width * 0.01);
+
+            // 縮小後的圖片大小應減去邊框的大小
+            $new_width = round(($width * 0.5) - (2 * $border_size));
+            $new_height = round(($height * 0.5) - (2 * $border_size));
 
             // 創建新圖片資源
             $thumb = imagecreatetruecolor($new_width, $new_height);
@@ -79,18 +82,36 @@
             // 將原圖片縮放並複製到新圖片
             imagecopyresampled($thumb, $source, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
+            // 創建一個帶邊框的圖片資源
+            $bordered_image = imagecreatetruecolor($new_width + 2 * $border_size, $new_height + 2 * $border_size);
+
+            // 定義四個邊框顏色
+            $color_top = imagecolorallocate($bordered_image, 255, 0, 0);     // 紅色
+            $color_right = imagecolorallocate($bordered_image, 255, 165, 0); // 橙色
+            $color_bottom = imagecolorallocate($bordered_image, 255, 255, 0); // 黃色
+            $color_left = imagecolorallocate($bordered_image, 0, 128, 0);    // 綠色
+
+            // 填充邊框區域
+            imagefilledrectangle($bordered_image, 0, 0, $new_width + 2 * $border_size, $border_size, $color_top); // 上
+            imagefilledrectangle($bordered_image, $new_width + $border_size, 0, $new_width + 2 * $border_size, $new_height + 2 * $border_size, $color_right); // 右
+            imagefilledrectangle($bordered_image, 0, $new_height + $border_size, $new_width + 2 * $border_size, $new_height + 2 * $border_size, $color_bottom); // 下
+            imagefilledrectangle($bordered_image, 0, 0, $border_size, $new_height + 2 * $border_size, $color_left); // 左
+
+            // 將縮小的圖片複製到帶邊框的圖片中
+            imagecopy($bordered_image, $thumb, $border_size, $border_size, 0, 0, $new_width, $new_height);
+
             // 儲存縮小後的圖片
             $output_file = $target_dir . "resized_" . basename($target_file);
             switch ($imageFileType) {
                 case 'jpg':
                 case 'jpeg':
-                    imagejpeg($thumb, $output_file);
+                    imagejpeg($bordered_image, $output_file);
                     break;
                 case 'png':
-                    imagepng($thumb, $output_file);
+                    imagepng($bordered_image, $output_file);
                     break;
                 case 'gif':
-                    imagegif($thumb, $output_file);
+                    imagegif($bordered_image, $output_file);
                     break;
             }
 
